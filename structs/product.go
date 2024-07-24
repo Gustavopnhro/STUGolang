384b6a54259ebd623c3fbb5c440805097adac51d
Gustavo <gustavo.pnhro@gmail.com>
2024-07-24 13:36:19 -0300

    fix: Change package to structs and change type of ID to allow uuid

--- structs/product.go
+++ structs/product.go
@@ -1,8 +1,10 @@
-package models
+package structs
+
+import "github.com/google/uuid"
 
 type Product struct {
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-	Stock int     `json:"stock"`
-	ID    string  `json:"id"`
+	Name  string    `json:"name"`
+	Price float64   `json:"price"`
+	Stock int       `json:"stock"`
+	ID    uuid.UUID `json:"id"`
 }
